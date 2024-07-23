4104ac276985c5b34e6531d2ec794b0904adcb13
PreetaeshSrivastava <148902015+PreetaeshSrivastava@users.noreply.github.com>
2024-07-23 22:57:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,135 @@
+"""
+This script fits a neural network to the output of an auditory filterbank.
+"""
+import datetime
+from datetime import timedelta
+import fire
+import h5py
+import numpy as np
+import os
+import pandas as pd
+import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import ModelCheckpoint
+import sys
+import time
+import torch
+
+import student
+import teacher
+
+MAX_EPOCHS = 100
+SAMPLES_PER_EPOCH = 8000
+
+# Set multiprocessing start method only once at the beginning
+if torch.cuda.is_available():
+    torch.multiprocessing.set_start_method("spawn", force=True)
+    
+def run(sav_dir, domain, arch, init_id, batch_size, job_id):
+    # Print header
+    start_time = int(time.time())
+    print("Job ID: " + str(job_id))
+    print(str(datetime.datetime.now()) + " Start.")
+    print(__doc__ + "\n")
+    print("\n".join([sav_dir, domain, arch, str(init_id)]) + "\n")
+
+    # Print version numbers
+    for module in [h5py, np, pd, pl, torch]:
+        print("{} version: {:s}".format(module.__name__, module.__version__))
+    print("")
+    sys.stdout.flush()
+
+    # Create model directory
+    model_dir = os.path.join(sav_dir, "models", domain)
+    model_sav_path = os.path.join(model_dir, str(job_id))
+    os.makedirs(model_sav_path, exist_ok=True)
+    pred_path = os.path.join(model_sav_path, "predictions.npy")
+
+    # Initialize dataset
+    if domain == "synth":
+        dataset = teacher.CQTSineDataModule(domain=domain, batch_size=batch_size)
+    else:
+        dataset = teacher.SpectrogramDataModule(
+            sav_dir=sav_dir, domain=domain, batch_size=batch_size)
+    print(str(datetime.datetime.now()) + " Finished initializing dataset")
+
+    if torch.cuda.is_available():
+        accelerator = "gpu"
+        devices = -1
+        #torch.multiprocessing.set_start_method("spawn")
+        strategy = 'ddp_notebook'
+    else:
+        accelerator = "cpu"
+        devices = "auto"
+        strategy = 'ddp_notebook'
+        
+    print("Current device: ", accelerator)
+
+    # Initialize model
+    constructor = getattr(student, arch)
+    spec = teacher.HYPERPARAMS[domain]
+    model = constructor(spec)
+    print(str(datetime.datetime.now()) + " Finished initializing model")
+
+    # Setup checkpoints and Tensorboard logger
+    checkpoint_cb = ModelCheckpoint(
+        dirpath=model_sav_path,
+        monitor="val_loss",
+        save_last=True,
+        filename="best",
+        save_weights_only=False,
+    )
+    tb_logger = pl_loggers.TensorBoardLogger(
+        save_dir=os.path.join(model_sav_path, "logs")
+    )
+
+    # Setup trainer
+    steps_per_epoch = SAMPLES_PER_EPOCH // batch_size
+    max_steps = steps_per_epoch * MAX_EPOCHS
+    strategy=pl.strategies.DDPStrategy(find_unused_parameters=True)
+    trainer = pl.Trainer(
+        accelerator=accelerator,
+        devices=devices,
+        max_epochs=MAX_EPOCHS,
+        max_steps=max_steps,
+        limit_train_batches=steps_per_epoch,
+        limit_val_batches=1.0,
+        limit_test_batches=1.0,
+        callbacks=[checkpoint_cb],
+        logger=tb_logger,
+        max_time=timedelta(hours=12),
+        strategy="auto",
+    )    
+
+    # Train
+    trainer.fit(model, dataset)
+
+    # Test
+    test_loss = trainer.test(model, dataset, verbose=False)
+    print("Model saved at: {}".format(model_sav_path))
+    print("Average test loss: {}".format(test_loss))
+    print("\n")
+
+    # Print elapsed time.
+    print(str(datetime.datetime.now()) + " Success.")
+    elapsed_time = time.time() - int(start_time)
+    elapsed_hours = int(elapsed_time / (60 * 60))
+    elapsed_minutes = int((elapsed_time % (60 * 60)) / 60)
+    elapsed_seconds = elapsed_time % 60.0
+    elapsed_str = "{:>02}:{:>02}:{:>05.2f}".format(
+        elapsed_hours, elapsed_minutes, elapsed_seconds
+    )
+    print("Total elapsed time: " + elapsed_str + ".")
+
+
+# Launch computation
+if __name__ == "__main__":
+    save_dir = "/content/drive/MyDrive/FittingAuditory/"
+    domain = "speech"
+    arch, init_id, batch_size, job_id = "Gabor1D", 0, 2, 1
+    fire.Fire(run(sav_dir=save_dir,
+                  domain=domain,
+                  arch=arch,
+                  init_id=init_id,
+                  batch_size=batch_size,
+                  job_id=job_id))
---
+++ student.py
@@ -0,0 +1,218 @@
+from collections import Counter
+#from murenn import DTCWTForward
+from murenn import DTCWT
+import numpy as np
+import torch
+import torchaudio
+import torch.nn as nn
+import pytorch_lightning as pl
+import torch.nn.utils.parametrize as P
+import torch.nn.functional as F
+from utils import GaborConv1d
+
+class Student(pl.LightningModule):
+    def __init__(self, spec):
+        super().__init__()
+        self.spec = spec
+        self.train_outputs = []
+        self.test_outputs = []
+        self.val_outputs = []
+        self.loss = nn.CosineSimilarity(dim=-1)
+        #self.loss = F.mse_loss
+
+    def step(self, batch, fold):
+        feat = batch['feature'].squeeze()
+        x = batch['x']
+        outputs = self(x)
+        #loss = F.mse_loss(outputs[:,1:,:], feat[:,1:,:]) 
+        loss = 0.5 * (1-self.loss(outputs[:,1:,:], feat[:,1:,:]).mean())
+        if fold == "train":
+            self.train_outputs.append(loss)
+        elif fold == "test":
+            self.test_outputs.append(loss)
+        elif fold == "val":
+            self.val_outputs.append(loss)
+        return {'loss': loss}
+    
+    def training_step(self, batch):
+        return self.step(batch, "train")
+    
+    def validation_step(self, batch, batch_idx):
+        return self.step(batch, "val")
+
+    def test_step(self, batch, batch_idx):
+        return self.step(batch, "test")
+    
+    def on_train_epoch_start(self):
+        self.train_outputs = []
+        self.test_outputs = []
+        self.val_outputs = []
+
+    def on_train_epoch_end(self):
+        avg_loss = torch.tensor(self.train_outputs).mean()
+        self.log('train_loss', avg_loss, prog_bar=False)
+    
+    def on_test_epoch_end(self):
+        avg_loss = torch.tensor(self.test_outputs).mean()
+        self.log('test_loss', avg_loss, prog_bar=False)
+
+    def on_validation_epoch_end(self):
+        avg_loss = torch.tensor(self.val_outputs).mean()
+        self.log('val_loss', avg_loss, prog_bar=False)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
+
+
+class TDFilterbank(Student):
+    def __init__(self, spec):
+        super().__init__(spec)
+
+        self.psi_real = torch.nn.Conv1d(
+            in_channels=1,
+            out_channels=spec["n_filters"],
+            kernel_size=spec["win_length"],
+            stride=spec["stride"],
+            padding=spec["win_length"]//2,
+            bias=False,
+        )
+
+        self.psi_imag = torch.nn.Conv1d(
+            in_channels=1,
+            out_channels=spec["n_filters"],
+            kernel_size=spec["win_length"],
+            stride=spec["stride"],
+            padding=spec["win_length"]//2,
+            bias=False,
+        )
+        
+    def forward(self, x):
+        x = x.reshape(x.shape[0], 1, x.shape[-1])
+        Wx_real = self.psi_real(x)
+        Wx_imag = self.psi_imag(x)
+        Ux = Wx_real * Wx_real + Wx_imag * Wx_imag
+        return Ux
+
+
+class Exp(nn.Module):
+    def forward(self, X):
+        return torch.exp(X)
+
+class Gabor1D(Student):
+    def __init__(self, spec, learn_amplitudes=True):
+        super().__init__(spec)
+        self.learn_amplitudes = learn_amplitudes
+        self.gaborfilter = GaborConv1d(
+            out_channels=2*spec['n_filters'], 
+            kernel_size=spec['win_length'],
+            stride=spec['stride'],
+            input_shape=None,
+            in_channels=1,
+            padding='same',
+            padding_mode='constant',
+            sample_rate=spec['sr'],
+            min_freq=spec['fmin'],
+            max_freq=spec['fmax'],
+            n_fft=spec['win_length'], #construct mel filters and determine initialization of gabor filters
+            normalize_energy=False,
+            bias=False,
+            sort_filters=True, #ascending order 
+            use_legacy_complex=False,
+            skip_transpose=False, #false means enable batch processing
+        )
+        self.learnable_scaling = nn.Conv1d(
+            in_channels=spec['n_filters'],
+            out_channels=spec['n_filters'],
+            kernel_size=1,
+            groups=spec['n_filters'],
+            bias=False
+        )
+        nn.init.constant_(
+            self.learnable_scaling.weight, val=0.0
+        )
+
+    def forward(self, x): 
+        Yx = self.gaborfilter(x) # (batch, time, 2*filters)
+        
+        # Squared modulus
+        Yx_real = Yx[:,:,:Yx.shape[-1]//2]
+        Yx_imag = Yx[:,:,Yx.shape[-1]//2:]
+        Ux = Yx_real*Yx_real + Yx_imag*Yx_imag
+        Ux = Ux.permute(0, 2, 1) # (batch, filters, time)
+
+        # Ensure positiveness of learned parameters
+        P.register_parametrization(
+            self.learnable_scaling, "weight", nn.Softmax(dim=1))
+        if self.learn_amplitudes: 
+            Ux = self.learnable_scaling(Ux)  
+        
+        return Ux
+    
+
+class MuReNN(Student):
+    def __init__(self, spec, Q_multiplier=16):
+        super().__init__(spec)
+        octaves = spec["octaves"]
+        Q_ctr = Counter(octaves)
+        self.J_psi = max(Q_ctr)
+        self.stride = spec["stride"]
+        
+        self.tfm = DTCWT(J=1+self.J_psi,alternate_gh=True, include_scale=False)
+
+        psis = []
+        for j in range(1+self.J_psi):
+            kernel_size = Q_multiplier*Q_ctr[j]
+            if j == 0:
+                stride_j = spec["stride"]
+            else:
+                stride_j = spec["stride"] // (2**(j-1))
+            psi = torch.nn.Conv1d(
+                in_channels=1,
+                out_channels=Q_ctr[j],
+                kernel_size=kernel_size,
+                stride=stride_j,
+                bias=False,
+                padding=kernel_size//2)
+            psis.append(psi)
+            
+        self.psis = torch.nn.ParameterList(psis)
+
+    def forward(self, x):
+        x = x.reshape(x.shape[0], 1, x.shape[-1])
+        _, x_levels = self.tfm.forward(x)
+        Ux = []
+        
+        for j_psi in range(1+self.J_psi):
+            x_level = x_levels[j_psi].type(torch.complex64) / (2**j_psi)
+            Wx_real = self.psis[j_psi](x_level.real)
+            Wx_imag = self.psis[j_psi](x_level.imag)
+            Ux_j = Wx_real * Wx_real + Wx_imag * Wx_imag
+            Ux_j = torch.real(Ux_j)
+            if j_psi == 0:
+                N_j = Ux_j.shape[-1]
+            else:
+                Ux_j = Ux_j[:, :, :N_j]
+            Ux.append(Ux_j)
+
+        Ux = torch.cat(Ux, axis=1)
+
+        # Flip j axis so that frequencies range from low to high
+        Ux = torch.flip(Ux, dims=(-2,))
+        return Ux
+    
+    def step(self, batch, fold):
+        feat = batch['feature'].squeeze()
+        x = batch['x']
+        outputs = self(x)
+        if outputs.shape[-2] + 1 == feat.shape[-2]:
+            loss = 0.5 * (1-self.loss(outputs, feat[:,1:,:]).mean())
+        else:
+            loss = 0.5 * (1-self.loss(outputs[:,1:,:], feat[:,1:,:]).mean())
+        
+        if fold == "train":
+            self.train_outputs.append(loss)
+        elif fold == "test":
+            self.test_outputs.append(loss)
+        elif fold == "val":
+            self.val_outputs.append(loss)
+        return {'loss': loss}
